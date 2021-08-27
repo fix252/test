e24bdc8c5769600d6e0607f099d9714634b2f316
fix252 <81397236+fix252@users.noreply.github.com>
2021-08-27 16:52:20 +0800

    Update serverinfo.sh

--- serverinfo.sh
+++ serverinfo.sh
@@ -25,5 +25,7 @@ else
   echo -e "${red}Server OS could not be detected.${default}`cat /etc/issue`"
 fi
 
+echo -e "${blue}CPU Cores: ${default}`cat /proc/cpuinfo | grep processor | wc -l`"
+
 echo -e "${blue} 高亮蓝字 ${default} 默认配色" 
 echo -e "${yellow} 高亮黄字 ${default} 默认配色"
