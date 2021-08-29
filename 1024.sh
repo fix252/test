287002e08f9216d998d85be05ef063f90a0c4717
fix252 <81397236+fix252@users.noreply.github.com>
2021-08-29 15:14:41 +0800

    Update 1024.sh

--- 1024.sh
+++ 1024.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-User="584544"
-EXT="75e07"
+User="xxxx"
+EXT="xxxx"
 Link="https://t66y.com/index.php?u=${User}&ext=${EXT}"
 echo "Link===============${Link}"
 
