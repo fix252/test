d6bb773c67ffbb1eacb1b62dfa95cc8bfc3bca45
fix252 <81397236+fix252@users.noreply.github.com>
2021-09-23 16:35:19 +0800

    Update optimization.sh

--- optimization.sh
+++ optimization.sh
@@ -20,15 +20,19 @@ if grep -q "^export HISTTIMEFORMAT" ${Profile}; then
   echo -e "${green}Command history was optimized. Won't re-do it."
 else  
   cat >> ${Profile} << EOF
-         export HISTTIMEFORMAT="%F %T "
-         export HISTORY_FILE=/var/log/command.log
-         export PROMPT_COMMAND='{ date "+%y-%m-%d %T ## \$(who am i |awk "{print \\\$3,\\\$4,\\\$1,\\\$2,\\\$5}") ## \$(whoami) ## \$(history 1 | { read x cmd; echo "\$cmd"; })"; } >>\$HISTORY_FILE'
+    export HISTTIMEFORMAT="%F %T "
+    export HISTORY_FILE=/var/log/command.log
+    export PROMPT_COMMAND='{ date "+%y-%m-%d %T ## \$(who am i |awk "{print \\\$3,\\\$4,\\\$1,\\\$2,\\\$5}") ## \$(whoami) ## \$(history 1 | { read x cmd; echo "\$cmd"; })"; } >>\$HISTORY_FILE'
   EOF
   
   source ${Profile}
   echo -e "${green}Done for command history optimization."
 fi
 
-#2, Update prompt for CentOS in /etc/bashrc
-#echo "PS1='[ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] ]\$ '" >> /etc/bashrc
-#echo "alias ll='ls -alh'" >> /etc/bashrc
+# 2, Update prompt for CentOS in /etc/bashrc
+# cat >> /etc/bashrc << EOF
+# PS1='[ \[\e]0;\u@\h: \w\a\]\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] ]\$ '
+# alias ll='ls -alh'
+# EOF
+# echo 'PS1="[ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] ]\$ "' >> /etc/bashrc
+# echo "alias ll='ls -alh'" >> /etc/bashrc
