d363a23a66458faa43af21bdbcc40abc6b42ac23
fix252 <81397236+fix252@users.noreply.github.com>
2022-07-05 08:44:26 +0800

    Update BlockIP.sh

--- BlockIP.sh
+++ BlockIP.sh
@@ -4,14 +4,12 @@
 # Then theses IPs will be blocked by being added to file /etc/hosts.deny.
 
 DenyFile="/etc/hosts.deny"
-LogFile="/var/log/BlockIP.log"
+LogFile="/var/log/block_ip.log"
 
 #LoginHistory="/var/log/auth.log*"
 #IPs=`grep -i "failed" ${LoginHistory} | egrep -o "([0-9]{1,3}\.){3}[0-9]{1,3}" | sort -n | uniq -c | awk '$1>=10 {print $2}'`
 
 IPs=`lastb -a | awk '$2~/ssh/ {print $NF}'| sort -n | uniq -c | awk '$1>=10 {print $2}'`
-
-echo -e "`date +"%F %T"`" >> ${LogFile}
 
 for i in ${IPs}
 do
@@ -19,11 +17,9 @@ do
                 #echo -e "`date +"%F %T"`: Existing IP ${i}" >> ${LogFile}
                 :
         else
-                echo -e "Blocking IP ${i}" >> ${LogFile}
+                echo -e "`date +"%F %T"` Blocking IP: ${i}" >> ${LogFile}
                 
                 echo -e "\n#`date +"%F %T"`" >> ${DenyFile}
                 echo "sshd:${i}" >> ${DenyFile}
         fi
 done
-
-echo "=======================================================" >> ${LogFile}
