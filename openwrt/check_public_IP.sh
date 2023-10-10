a9e3336aad5ae6527d567568f94b7a48882788b4
fix252 <81397236+fix252@users.noreply.github.com>
2023-10-10 10:36:26 +0800

    Update check_public_IP.sh

--- openwrt/check_public_IP.sh
+++ openwrt/check_public_IP.sh
@@ -3,13 +3,11 @@
 # Install msmtp and update /etc/msmtprc first.
 # Add crontab task: */2 * * * * sh check_public_IP.sh
 
-history_ip="/root/device_ip.txt"
+# WAN interface name
+interface_name="pppoe-WAN"
 
-interface_ip=$(ifconfig pppoe-WAN | sed -n "2,2p" | awk '{print $2}' | awk -F : '{print $2}')
-outbound_ip=$(curl -s ifconfig.me)
-
-current_ip=$(curl -s ifconfig.me)
-last_ip=$(tail -1 ${history_ip} | awk '{print $2}')
+# Router name
+router_name=""
 
 # CloudFlare Token
 zone_id=""
@@ -18,28 +16,42 @@ record_id=""
 name=""       #FQDN
 result=""
 
-if [ -z ${last_ip} ] || [ ${last_ip} != ${current_ip} ]; then
-        echo -e "$(date +'%Y%m%d%H%M%S') ${current_ip}" >> ${history_ip}
-        # Update DNS record
-        update=$(curl -s --request PUT \
-                  --url "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
-                  --header "Content-Type: application/json" \
-                  --header "X-Auth-Key: " \
-                  --header "X-Auth-Email: " \
-                  --header "Authorization: Bearer ${api_token}" \
-                  --data "{
-                  \"content\": \"${current_ip}\",
-                  \"name\": \"${name}\",
-                  \"proxied\": false,
-                  \"type\": \"A\"
-                }")
-        
-        if echo ${update} | grep -q "\"success\":true";then
-                result="${current_ip}, and update dns record successfully."
-        else
-                result="${current_ip}, but failed to update dns record with error message\n${update}"
-        fi
+email_sender=""
+email_receiver=""    #space seperator for multiple receivers
 
-        #Send Email Notification
-        echo -e "Subject: New IP\n\n$(date +'%F %T %A %z'):\t${result}" | msmtp -f FROM_ADDRESS TO_ADDRESS_WHIT_SPACE_FOR_MULTI_RECEIVERS
+interface_ip=$(ifconfig ${interface_name} | sed -n "2,2p" | awk '{print $2}' | awk -F : '{print $2}')
+outbound_ip=$(curl -s ifconfig.me)
+history_ip="/root/device_ip.txt"
+
+if [ ${outbound_ip} == ${interface_ip} ]; then
+        current_ip=${interface_ip}
+        last_ip=$(tail -1 ${history_ip} | awk '{print $2}')
+        if [ -z ${last_ip} ] || [ ${last_ip} != ${current_ip} ]; then
+                echo -e "$(date +'%Y%m%d%H%M%S') ${current_ip}" >> ${history_ip}
+                # Update DNS record
+                update=$(curl -s --request PUT \
+                          --url "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
+                          --header "Content-Type: application/json" \
+                          --header "X-Auth-Key: " \
+                          --header "X-Auth-Email: " \
+                          --header "Authorization: Bearer ${api_token}" \
+                          --data "{
+                          \"content\": \"${current_ip}\",
+                          \"name\": \"${name}\",
+                          \"proxied\": false,
+                          \"type\": \"A\"
+                        }")
+        
+                if echo ${update} | grep -q "\"success\":true";then
+                        result="$(date +'%F %T %A %z'):\t${current_ip}, and update dns record successfully."
+                else
+                        result="$(date +'%F %T %A %z'):\t${current_ip}, but failed to update dns record with error message\n${update}"
+                fi
+                
+        fi
+else
+        result="$(date +'%F %T %A %z'):\tPublic IP Error: outbound IP ${outbound_ip} != interface IP ${interface_ip}."
 fi
+
+#Send Email Notification
+echo -e "Subject: New IP at ${router_name}\n\n${result}" | msmtp -f ${email_sender} ${email_receiver}
