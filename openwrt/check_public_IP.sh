6f75011276d55fcffc1e3698dabffbb463661e1a
fix252 <81397236+fix252@users.noreply.github.com>
2023-10-10 11:13:27 +0800

    Update check_public_IP.sh

--- openwrt/check_public_IP.sh
+++ openwrt/check_public_IP.sh
@@ -5,7 +5,7 @@
 # Add crontab task: */2 * * * * sh check_public_IP.sh
 
 # WAN interface name
-interface_name="pppoe-WAN"
+interface_name=""
 
 # Router name
 router_name=""
@@ -14,21 +14,23 @@ router_name=""
 zone_id=""
 api_token=""
 record_id=""
-name=""       #FQDN
-result=""
+record_name=""       #FQDN
 
 email_sender=""
 email_receiver=""    #space seperator for multiple receivers
+email_subject="Subject: New IP at ${router_name}"
+email_content=""     #Keep content null here.
 
 interface_ip=$(ifconfig ${interface_name} | sed -n "2,2p" | awk '{print $2}' | awk -F : '{print $2}')
 outbound_ip=$(curl -s ifconfig.me)
 history_ip="/root/device_ip.txt"
 
 if [ ${outbound_ip} == ${interface_ip} ]; then
-        current_ip=${interface_ip}
+        current_ip="${interface_ip}"
         last_ip=$(tail -1 ${history_ip} | awk '{print $2}')
         if [ -z ${last_ip} ] || [ ${last_ip} != ${current_ip} ]; then
                 echo -e "$(date +'%Y%m%d%H%M%S') ${current_ip}" >> ${history_ip}
+                
                 # Update DNS record
                 update=$(curl -s --request PUT \
                           --url "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
@@ -38,23 +40,22 @@ if [ ${outbound_ip} == ${interface_ip} ]; then
                           --header "Authorization: Bearer ${api_token}" \
                           --data "{
                           \"content\": \"${current_ip}\",
-                          \"name\": \"${name}\",
+                          \"name\": \"${record_name}\",
                           \"proxied\": false,
                           \"type\": \"A\"
                         }")
-        
-                if echo ${update} | grep -q "\"success\":true";then
-                        result="$(date +'%F %T %A %z'):\t${current_ip}, and update dns record successfully."
-                else
-                        result="$(date +'%F %T %A %z'):\t${current_ip}, but failed to update dns record with error message\n${update}"
-                fi
                 
+                if echo ${update} | grep -q "\"success\":true";then
+                        email_content="$(date +'%F %T %A %z'):\t${current_ip}, and update dns record successfully."
+                else
+                        email_content="$(date +'%F %T %A %z'):\t${current_ip}, but failed to update dns record with error message\n${update}"
+                fi
         fi
 else
-        result="$(date +'%F %T %A %z'):\tPublic IP Error: outbound IP ${outbound_ip} != interface IP ${interface_ip}."
+        email_content="$(date +'%F %T %A %z'):\tPublic IP Error: outbound IP ${outbound_ip} != interface IP ${interface_ip}."
 fi
 
 #Send Email Notification
-if [ ${result} ]; then
-        echo -e "Subject: New IP at ${router_name}\n\n${result}" | msmtp -f ${email_sender} ${email_receiver}
+if [ ${email_content} ]; then
+        echo -e "${email_subject}\n\n${email_content}" | msmtp -f ${email_sender} ${email_receiver}
 fi
