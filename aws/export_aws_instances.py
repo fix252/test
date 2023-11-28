b319098028e6adce28b20e9d821f4cba329770a3
fix252 <81397236+fix252@users.noreply.github.com>
2023-11-28 10:25:52 +0800

    Update export_aws_instances.py

--- aws/export_aws_instances.py
+++ aws/export_aws_instances.py
@@ -17,7 +17,7 @@ access_key = "xxxx"
 secret_access_key = "xxxx"
 #sgp, hk and fra are customized short names for each region.
 regions = {"ap-southeast-1": "sgp", "ap-east-1": "hk", "eu-central-1": "fra"}
-resources = ["ec2", "rds", "elasticache"]
+resources = ["ec2", "rds", "elasticache", "elbv2"]
 
 config = Config(proxies={})
 
@@ -36,11 +36,11 @@ for z in regions:
             #Export ec2 instances
             response = client.describe_instances()
             now = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            filename = account_owner + "_" + regions[z] + "_" + r + "_" + now + ".csv"
+            filename = now + "_" + account_owner + "_" + regions[z] + "_" + r + ".csv"
             print(filename)
             with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
                 writer = csv.writer(csvf)
-                csv_head = ["Index", "InstanceName", "Project", "Group", "APP", "CreateTime", "LaunchTime", "InstanceID", "InstanceType", "PublicIP", "PrivateIP", "State", "KeyName", "ImageID", "Zone", "Archi", "Platform"]
+                csv_head = ["Index", "InstanceName", "Project", "Group", "APP", "Owner", "CreateTime", "LaunchTime", "InstanceID", "InstanceType", "PublicIP", "PrivateIP", "State", "KeyName", "ImageID", "Zone", "Archi", "Platform", "Location"]
                 writer.writerow(csv_head)
                 index = 0
                 for i in response['Reservations']:
@@ -58,6 +58,7 @@ for z in regions:
                         project = ""
                         group = ""
                         app = ""
+                        owner = ""
                         for dic in j['Tags']:
                             #Default tag for instance name.
                             if dic['Key'] == 'Name':
@@ -69,14 +70,15 @@ for z in regions:
                                 group = dic['Value']
                             if dic['Key'] == 'app':
                                 app = dic['Value']
-                                
-                        row_cvs = [index, name, project, group, app, j['UsageOperationUpdateTime'], j['LaunchTime'], j['InstanceId'], j['InstanceType'], j['PublicIpAddress'], j['PrivateIpAddress'], j['State']['Name'], j['KeyName'], j['ImageId'], j['Placement']['AvailabilityZone'], j['Architecture'], j['PlatformDetails']]
+                            if dic['Key'] == 'owner':
+                                owner = dic['Value']
+                        row_cvs = [index, name, project, group, app, owner, j['UsageOperationUpdateTime'], j['LaunchTime'], j['InstanceId'], j['InstanceType'], j['PublicIpAddress'], j['PrivateIpAddress'], j['State']['Name'], j['KeyName'], j['ImageId'], j['Placement']['AvailabilityZone'], j['Architecture'], j['PlatformDetails'], regions[z]]
                         writer.writerow(row_cvs)
             
             #Export reserved ec2 instances
             response = client.describe_reserved_instances()
             now = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            filename = account_owner + "_" + regions[z] + "_" + r + "_reserved_"+ now + ".csv"
+            filename = now + "_" + account_owner + "_" + regions[z] + "_" + r + "_reserved.csv"
             print(filename)
             with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
                 writer = csv.writer(csvf)
@@ -93,11 +95,11 @@ for z in regions:
             #Export rds instances
             response = client.describe_db_instances()
             now = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            filename = account_owner + "_" + regions[z] + "_" + r + "_" + now + ".csv"
+            filename = now + "_" + account_owner + "_" + regions[z] + "_" + r + ".csv"
             print(filename)
             with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
                 writer = csv.writer(csvf)
-                csv_head = ["Index", "DBInstanceIdentifier", "Project", "Group", "APP", "Role", "DBInstanceClass", "Engine", "EngineVersion", "Status", "Storage", "CreateTime", "Zone", "MultiAZ"]
+                csv_head = ["Index", "DBInstanceIdentifier", "Project", "Group", "APP", "Owner", "Role", "DBInstanceClass", "Engine", "EngineVersion", "Status", "Storage", "CreateTime", "Zone", "MultiAZ", "Location"]
                 writer.writerow(csv_head)
                 
                 index = 0
@@ -110,6 +112,7 @@ for z in regions:
                     project = ""
                     group = ""
                     app = ""
+                    owner = ""
                     for dic in i['TagList']:
                         #Customized tags, case sensitive.
                         if dic['Key'] == 'project':
@@ -118,14 +121,15 @@ for z in regions:
                             group = dic['Value']
                         if dic['Key'] == 'app':
                             app = dic['Value']
-                    
-                    row_cvs = [index, i['DBInstanceIdentifier'], project, group, app, role, i['DBInstanceClass'], i['Engine'], i['EngineVersion'], i['DBInstanceStatus'], i['AllocatedStorage'], i['InstanceCreateTime'], i['AvailabilityZone'], i['MultiAZ']]
+                        if dic['Key'] == 'owner':
+                            owner = dic['Value']
+                    row_cvs = [index, i['DBInstanceIdentifier'], project, group, app, owner, role, i['DBInstanceClass'], i['Engine'], i['EngineVersion'], i['DBInstanceStatus'], i['AllocatedStorage'], i['InstanceCreateTime'], i['AvailabilityZone'], i['MultiAZ'], regions[z]]
                     writer.writerow(row_cvs)
             
             #Export reserved rds instances
             response = client.describe_reserved_db_instances()
             now = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            filename = account_owner + "_" + regions[z] + "_" + r + "_reserved_"+ now + ".csv"
+            filename = now + "_" + account_owner + "_" + regions[z] + "_" + r + "_reserved.csv"
             print(filename)
             with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
                 writer = csv.writer(csvf)
@@ -150,7 +154,7 @@ for z in regions:
             print(filename)
             with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
                 writer = csv.writer(csvf)
-                csv_head = ["Index", "ClusterName", "Project", "Group", "APP", "NodeCount", "CacheNodeType", "Engine", "EngineVersion", "Status", "CreateTime", "MultiAZ"]
+                csv_head = ["Index", "ClusterName", "Project", "Group", "APP", "Owner", "NodeCount", "CacheNodeType", "Engine", "EngineVersion", "Status", "CreateTime", "MultiAZ", "Location"]
                 writer.writerow(csv_head)
                 
                 index = 0
@@ -170,6 +174,7 @@ for z in regions:
                     project = ""
                     group = ""
                     app = ""
+                    owner = ""
                     for dic in response_tmp['TagList']:
                         # Customized tags, case sensitive.
                         if dic['Key'] == 'project':
@@ -178,8 +183,10 @@ for z in regions:
                             group = dic['Value']
                         if dic['Key'] == 'app':
                             app = dic['Value']
+                        if dic['Key'] == 'owner':
+                            owner = dic['Value']
                     
-                    row_cvs = [index, i['ReplicationGroupId'], project, group, app, len(i['MemberClusters']) ,i['CacheNodeType'], engine, engineVersion, i['Status'], createTime, i['MultiAZ']]
+                    row_cvs = [index, i['ReplicationGroupId'], project, group, app, owner, len(i['MemberClusters']) ,i['CacheNodeType'], engine, engineVersion, i['Status'], createTime, i['MultiAZ'], regions[z]]
                     writer.writerow(row_cvs)
                     
             #Export reserved elasticache instances
@@ -200,3 +207,94 @@ for z in regions:
                     
                     row_cvs = [index, i['ReservedCacheNodeId'], i['CacheNodeType'], i['CacheNodeCount'], i['StartTime'], i['Duration']/86400, endtime, i['ProductDescription'], i['OfferingType'], i['State'], i['FixedPrice']]
                     writer.writerow(row_cvs)
+
+        #Export elb instances
+        elif r == "elbv2":
+            #Export elb instances
+            response = client.describe_load_balancers()
+            now = time.strftime("%Y%m%d%H%M%S", time.localtime())
+            filename = now + "_" + account_owner + "_" + regions[z] + "_" + r + ".csv"
+            print(filename)
+            with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
+                writer = csv.writer(csvf)
+                csv_head = ["Index", "Name", "Project", "Group", "APP", "Owner", "DNSName", "State", "Type", "CreateTime", "Location"]
+                writer.writerow(csv_head)
+                
+                index = 0
+                for i in response['LoadBalancers']:
+                    index = index + 1
+                    
+                    project = ""
+                    group = ""
+                    app = ""
+                    owner = ""
+                    response_tmp = client.describe_tags(ResourceArns=[i['LoadBalancerArn'],])
+                    for j in response_tmp['TagDescriptions'][0]['Tags']:
+                        # Customized tags, case sensitive.
+                        if j['Key'] == 'project':
+                            project = j['Value']
+                        if j['Key'] == 'group':
+                            group = j['Value']
+                        if j['Key'] == 'app':
+                            app = j['Value']
+                        if j['Key'] == 'owner':
+                            owner = j['Value']
+                    
+                    row_cvs = [index, i['LoadBalancerName'], project, group, app, owner, i['DNSName'], i['State']['Code'], i['Type'], i['CreatedTime'], regions[z]]
+                    writer.writerow(row_cvs)
+
+#Get S3 Buckets
+client = boto3.client(
+    's3',
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_access_key,
+    config=config,
+    )
+response = client.list_buckets()
+now = time.strftime("%Y%m%d%H%M%S", time.localtime())
+filename = now + "_" + account_owner + "_s3_bucket.csv"
+print(filename)
+with open(filename, "w", encoding="utf-8-sig", newline="") as csvf:
+    writer = csv.writer(csvf)
+    csv_head = ["Index", "BucketName", "Project", "Group", "APP", "Owner", "CreateTime", "Location"]
+    writer.writerow(csv_head)
+    index = 0
+    for i in response['Buckets']:
+        index = index + 1
+        name = i['Name']
+        createtime = i['CreationDate']
+        
+        project = ""
+        group = ""
+        app = ""
+        owner = ""
+        
+        try:
+            response1 = client.get_bucket_tagging(
+                Bucket=name,
+            )
+            for dic in response1['TagSet']:
+                # Customized tags, case sensitive.
+                if dic['Key'] == 'project':
+                    project = dic['Value']
+                if dic['Key'] == 'group':
+                    group = dic['Value']
+                if dic['Key'] == 'app':
+                    app = dic['Value']
+                if dic['Key'] == 'owner':
+                    owner = dic['Value']
+        except:
+            pass
+            
+        location = ""
+        try:
+            response2 = client.get_bucket_location(
+                    Bucket=name,
+            )
+            location = response2['LocationConstraint']
+        except:
+            pass
+        
+        print(index, name, project, group, app, owner, createtime, location)
+        row_cvs = [index, name, project, group, app, owner, createtime, location]
+        writer.writerow(row_cvs)
